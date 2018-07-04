892c6f03c01abae2278bf2116813ca2829b5d4f3
Chananya Freiman <spectralcanine@gmail.com>
2018-07-05 01:00:10 +0300

    Simplified the wc3stats command
    I wanted to add more options to it, and it was becoming more and more messy. So it instead just immediately sends the links for the stats, rather than fetching and parsing the DOM.

--- commands/wc3stats.js
+++ commands/wc3stats.js
@@ -1,7 +1,6 @@
-let fetch = require('node-fetch'),
-    JSDOM = require('jsdom').JSDOM;
-
 function parseRealm(realm) {
+    realm = realm.toLowerCase();
+
     if ('northrend'.startsWith(realm)) {
         return 'northrend';
     } else if ('lordaeron'.startsWith(realm)) {
@@ -15,41 +14,7 @@ function parseRealm(realm) {
     }
 }
 
-function parseRace(race) {
-    if ('random'.startsWith(race) || race === 'rnd' || race === 'rdm') {
-        return 'random';
-    } else if ('human'.startsWith(race)) {
-        return 'human';
-    } else if ('orc'.startsWith(race)) {
-        return 'orc';
-    } else if ('undead'.startsWith(race) || race === 'ud') {
-        return 'undead';
-    } else if ('nightelf'.startsWith(race) || race === 'ne') {
-        return 'nightelf';
-    } else if ('total'.startsWith(race)) {
-        return 'total';
-    }
-
-    return '';
-}
-
-function getBnetRaceRow(rows, race) {
-    for (let row of rows) {
-        let rowRace = row.querySelector('.rankingHeader').textContent;
-
-        if ((race === 'total' && rowRace === 'Total:') ||
-            (race === 'random' && rowRace === 'Random:') ||
-            (race === 'human' && rowRace === 'Human:') ||
-            (race === 'orc' && rowRace === 'Orc:') ||
-            (race === 'undead' && rowRace === 'Undead:') ||
-            (race === 'nightelf' && rowRace === 'Night Elf:')) {
-            return row;
-        }
-    }
-}
-
-// args: <mode> <user> <user|race|all>
-// e.g.: solo 
+// args: <realm> <user>
 module.exports = {
     name: 'wc3stats',
 
@@ -60,110 +25,19 @@ module.exports = {
             args = data.args;
 
         if (args.length < 2) {
-            channel.message(`@${userName}, usage: ${command.name} <realm> <user> [<race>].`);
-            channel.message(`gets the solo ladder win:loss of the given user at the given realm.`);
+            channel.message(`@${userName}, usage: ${command.name} <realm> <user>`);
             return;
         }
 
-        let arg1 = args[0].toLowerCase(),
-            arg2 = args[1],
-            realm = parseRealm(arg1);
+        let realm = parseRealm(args[0]),
+            player = args[1].toLowerCase();
 
         if (realm === '') {
-            channel.message(`@${userName}, '${arg1}' is not a valid realm`);
-            return;
-        }
-
-        let arg3 = 'total';
-
-        if (args.length > 2) {
-            arg3 = args[2].toLowerCase();
-        }
-
-        if (realm === 'w3arena') {
-            fetch(`http://tft.w3arena.net/profile/${arg2}`)
-                .then((response) => {
-                    return response.text();
-                })
-                .then((text) => {
-                    let document = new JSDOM(text).window.document,
-                        profileContainer = document.querySelector('.profileContainer');
-
-                    if (!profileContainer) {
-                        channel.message(`@${userName}, '${arg2}' is not a valid w3arena user.`);
-                        return;
-                    }
-
-                    let name = profileContainer.querySelector('.basicData > .innerDescriptionBox > div').textContent.trim();
-
-                    let leftColumn = profileContainer.querySelector('.leftColumn');
-
-                    let stats = leftColumn.querySelector('.tabContainer');
-
-                    let soloStats = stats.querySelector('#solo');
-                    let soloRank = soloStats.querySelector('.rank > strong').textContent;
-                    //console.log('solo rank', soloRank);
-                    let soloWinLoss = soloStats.querySelector('.winLossContainer');
-                    let soloWins = soloWinLoss.querySelector('.winBar').textContent.split(' ')[0];
-                    //console.log('solo wins', soloWins);
-                    let soloLosses = soloWinLoss.querySelector('.lossBar').textContent.split(' ')[0];
-                    //console.log('solo losses', soloLosses);
-                    let soloLevel = soloStats.querySelector('.level').textContent.split(' ')[1];
-                    //console.log('solo level', soloLevel);
-                    let soloExperience = soloStats.querySelector('.progressContainer > .experience').textContent.split(' ')[1];
-                    //console.log('solo exp', soloExperience);
-                    let rtStats = stats.querySelector('#randomTeam');
-                    // TODO: get RT stats
-
-                    let atStats = stats.querySelector('#arrangedTeam');
-                    // TODO: get AT stats
-
-                    let gameSummary = leftColumn.querySelector('.gameSummary');
-
-                    channel.message(`@${userName}, ${arg2} at ${realm} ${soloWins}:${soloLosses}.`);
-                });
+            channel.message(`@${userName}, "${arg1}" is not a valid realm!`);
+        } else if (realm === 'w3arena') {
+            channel.message(`@${userName}, http://tft.w3arena.net/profile/${player}`);
         } else {
-            fetch(`http://classic.battle.net/war3/ladder/w3xp-player-profile.aspx?Gateway=${realm}&PlayerName=${arg2}`)
-                .then((response) => {
-                    return response.text();
-                })
-                .then((text) => {
-                    let document = new JSDOM(text).window.document,
-                        ladderTable = document.querySelector('.ladderTableGray');
-
-                    if (ladderTable) {
-                        let race = parseRace(arg3);
-
-                        // If the user supplied a race but it's invalid, say so.
-                        if (race === '') {
-                            channel.message(`@${userName}, '${arg3}' is not a valid race.`);
-                            return;
-                        }
-
-                        let rows = ladderTable.querySelectorAll('tr'),
-                            row = getBnetRaceRow(rows, race);
-
-                        // If the row is not found, it means the player hasn't played with that race.
-                        if (!row) {
-                            channel.message(`@${userName}, ${arg2} at ${realm} as ${race} - 0:0.`);
-                            return;
-                        }
-
-                        let rankingRows = row.querySelectorAll('.rankingRow'),
-                            wins = rankingRows[0].textContent,
-                            losses = rankingRows[1].textContent;
-
-                        // Either the user supplied 'total', or he didn't support any race.
-                        // In both there is no reason to write the race.
-                        if (arg3 === 'total') {
-                            channel.message(`@${userName}, ${arg2} at ${realm} - ${wins}:${losses}.`);
-                        } else {
-                            channel.message(`@${userName}, ${arg2} at ${realm} as ${race} - ${wins}:${losses}.`);
-                        }
-                    } else {
-                        channel.message(`@${userName}, ${arg2} at ${realm} is not a valid ${realm} user.`);
-                    }
-                });
+            channel.message(`@${userName}, http://classic.battle.net/war3/ladder/w3xp-player-reports-overall.aspx?Gateway=${realm}&PlayerName=${player}`);
         }
     }
 };
